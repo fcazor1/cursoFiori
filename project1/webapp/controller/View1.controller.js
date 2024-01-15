23d65bf353adfb447879694e64090ab2d2bed873
Fadel Antonio Cazor <fcazor1@hotmail.com>
2024-01-15 14:15:37 +0000

    primeros cambios en la app

--- project1/webapp/controller/View1.controller.js
+++ project1/webapp/controller/View1.controller.js
@@ -9,11 +9,32 @@ sap.ui.define([
 
         return Controller.extend("neoris.cursos.fiori.test.project1.controller.View1", {
             onInit: function () {
-
+                this.loadFragment({
+                    name: "neoris.cursos.fiori.test.project1.view.ComboBox"
+                });
+                var sPath = "/MatWebComboSet";
+                var oModelOdata = this.getOwnerComponent().getModel();
+                var ofilter = new satisfies.ui.model.Filter("TipoDato", sap.ui.model.FilterOperator.EQ, 'PAIS');
+                oModelOdata.read(sPath, {
+                    filters: [ofilter],
+                    success: function (oData, response){
+                        var oModelPaises = new sap.ui.model.json.JSONModel();
+                        if (oData.results.length>0) {
+                            oModelPaises.setData(oData.results);
+                        }else{
+                            data = {};
+                            oModelPaises.setData(data);
+                        }
+                        this.getView().setModel(oModelPaises, "Paises");
+                    },
+                    error: function (oError){
+                        sap.m.MessageToast.show(oError.message);
+                    }
+                });
             },
 
             onPress: function (oEvent) {
-                alert(oEvent);
+                alert(oEvent.oSource.getSelectedKey());
             }
         });
     });
